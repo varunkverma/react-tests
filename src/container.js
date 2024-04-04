0580e930dcd110a36f2be867e7cf1d531b7e778c
Varun Verma <varunkverma@gmail.com>
2024-04-04 09:23:39 +0000

    base questio2n

--- src/container.js
+++ src/container.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import Plug1 from "./plug1";
 import Plug2 from "./plug2";
 
 const ContainerCom = () => {
+    const containerRef = useRef();
+    const handleAction = () => {
+        // ask child
+        // hint
+        if(containerRef.current && containerRef.current.actionToDo){
+            containerRef.current.actionToDo();
+        } else {
+            console.log("No Idea?")
+        }        
+    }
     return (
         <div className="container">
             {/* <Plug1 /> */}
             <Plug2 />
-            <button className="actionBtn">Change</button>
+            <button className="actionBtn" onClick={handleAction}>Change</button>
         </div>
     )
 }
